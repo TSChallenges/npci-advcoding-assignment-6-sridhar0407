86fbb6132f3129c342f083d98c0b433d806c3ec4
sridhar0407 <vsridhar77@gmail.com>
2025-02-13 18:53:26 +0530

    AppConfig

---
+++ ex2/src/main/java/com/mystore/app/AppConfig.java
@@ -0,0 +1,25 @@
+package com.mystore.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Product product1(){
+        Product p1 = new Product();
+        p1.setId(1);
+        p1.setName("Tumbler");
+        return p1;
+    }
+
+    @Bean
+    public  Product product2(){
+        Product p2 = new Product();
+        p2.setId(2);
+        p2.setName("Bucket");
+        return p2;
+    }
+}
